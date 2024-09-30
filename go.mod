9f6c9b2027bf11bc3b3768ccb5d0bed04c039c6b
cyrildou <cyrildou@live.cn>
2024-09-30 10:22:26 +0800

    add function to get group spread diff value

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/asaka1234/go-mtmanapi
 
 go 1.22.2
+
+require github.com/shopspring/decimal v1.4.0 // indirect
--- win32/pkg/utils.go
+++ win32/pkg/utils.go
@@ -1,6 +1,10 @@
 package pkg
 
-import "github.com/asaka1234/go-mtmanapi/win32/mtmanapi"
+import (
+	"github.com/asaka1234/go-mtmanapi/win32/mtmanapi"
+	"github.com/shopspring/decimal"
+	"math"
+)
 
 // 获取32/64位
 func GetSysVersion() int {
@@ -12,4 +16,35 @@ func GetSysVersion() int {
 func GetApiVersion() int {
 	//(a << 16) | b
 	return (mtmanapi.ManAPIProgramVersion << 16) | mtmanapi.ManAPIProgramBuild
+}
+
+// 获取组点
+func GetGroupSpreadDiff(manager mtmanapi.CManagerInterface, req mtmanapi.RequestInfo) (decimal.Decimal, decimal.Decimal) {
+	//增加组点
+	var symbolInfo mtmanapi.ConSymbol
+	symbol := req.GetTrade().GetSymbol()
+	group := req.GetGroup()
+	manager.SymbolGet(symbol, symbolInfo)
+	xtype := symbolInfo.GetXtype()
+	digit := symbolInfo.GetDigits()
+
+	var groupInfo mtmanapi.ConGroup
+	manager.GroupRecordGet(group, groupInfo)
+
+	secGroups := groupInfo.GetSecgroups()
+	singleGroup := mtmanapi.ConGroupSecArray_getitem(secGroups, int64(xtype))
+	spreadDiff := singleGroup.GetSpread_diff() //获取组点
+
+	//数量
+	spreadBid := spreadDiff / 2
+	spreadAsk := spreadDiff - spreadBid
+
+	//基本单位
+	denominator := math.Pow(0.1, float64(digit))
+
+	//两个方向各自的组点值
+	bidVal := decimal.NewFromInt(int64(spreadBid)).Mul(decimal.NewFromFloat(denominator))
+	askVal := decimal.NewFromInt(int64(spreadAsk)).Mul(decimal.NewFromFloat(denominator))
+
+	return bidVal, askVal
 }
