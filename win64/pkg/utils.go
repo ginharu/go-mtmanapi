04f225c965ca151a210eb427f7565615b791bfb9
cyrildou <cyrildou@live.cn>
2024-09-30 10:29:21 +0800

    add function to get group spread diff value

--- win64/pkg/utils.go
+++ win64/pkg/utils.go
@@ -20,33 +20,9 @@ func GetApiVersion() int {
 
 // 获取组点
 func GetGroupSpreadDiff(manager mtmanapi.CManagerInterface, req mtmanapi.RequestInfo) (decimal.Decimal, decimal.Decimal) {
-	//增加组点
-	var symbolInfo mtmanapi.ConSymbol
 	symbol := req.GetTrade().GetSymbol()
 	group := req.GetGroup()
-	manager.SymbolGet(symbol, symbolInfo)
-	xtype := symbolInfo.GetXtype()
-	digit := symbolInfo.GetDigits()
-
-	var groupInfo mtmanapi.ConGroup
-	manager.GroupRecordGet(group, groupInfo)
-
-	secGroups := groupInfo.GetSecgroups()
-	singleGroup := mtmanapi.ConGroupSecArray_getitem(secGroups, int64(xtype))
-	spreadDiff := singleGroup.GetSpread_diff() //获取组点
-
-	//数量
-	spreadBid := spreadDiff / 2
-	spreadAsk := spreadDiff - spreadBid
-
-	//基本单位
-	denominator := math.Pow(0.1, float64(digit))
-
-	//两个方向各自的组点值
-	bidVal := decimal.NewFromInt(int64(spreadBid)).Mul(decimal.NewFromFloat(denominator))
-	askVal := decimal.NewFromInt(int64(spreadAsk)).Mul(decimal.NewFromFloat(denominator))
-
-	return bidVal, askVal
+	return GetGroupSpreadDiffBySymbol(manager, group, symbol)
 }
 
 // 获取组点
