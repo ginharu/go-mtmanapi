5213781439a9ef66d66152c79743e30832b0368a
cyrildou <cyrildou@live.cn>
2024-10-08 11:58:39 +0800

    test1

--- win64/mtmanapi/mtmanapi_wrap.cxx
+++ win64/mtmanapi/mtmanapi_wrap.cxx
@@ -25514,7 +25514,6 @@ ConSymbol *_wrap_CManagerInterface_CfgRequestSymbol_mtmanapi_6b0ddf25a5f93138(CM
   arg2 = *(int **)&_swig_go_1; 
   
   result = (ConSymbol *)(arg1)->CfgRequestSymbol(arg2);
-  printf("_wrap_CManagerInterface_CfgRequestSymbol_mtmanapi_6b0ddf25a5f93138\n")
   *(ConSymbol **)&_swig_go_result = (ConSymbol *)result; 
   return _swig_go_result;
 }
